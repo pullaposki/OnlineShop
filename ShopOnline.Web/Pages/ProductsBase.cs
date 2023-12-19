63ce3c00ddb43395d12a4b5254622615dc918318
pullaposki <pullaposki@windowslive.com>
2023-12-19 11:57:41 +0200

    Refactored Products view to display products grouped by categories.

--- ShopOnline.Web/Pages/ProductsBase.cs
+++ ShopOnline.Web/Pages/ProductsBase.cs
@@ -4,16 +4,30 @@ using ShopOnline.Web.Services.Contracts;
 
 namespace ShopOnline.Web.Pages
 {
-	public class ProductsBase : ComponentBase
-	{
-		[Inject]
-		public IProductService ProductService { get; set; }
+    public class ProductsBase : ComponentBase
+    {
+        [Inject] public IProductService ProductService { get; set; }
 
-		public IEnumerable<ProductDto> Products { get; set; }
+        public IEnumerable<ProductDto> Products { get; set; }
 
-		protected override async Task OnInitializedAsync()
-		{
-			Products = await ProductService.GetItems();
-		}
-	}
+        protected override async Task OnInitializedAsync()
+        {
+            Products = await ProductService.GetItems();
+        }
+
+        protected IOrderedEnumerable<IGrouping<int, ProductDto>> GetGroupedProductsByCategory()
+        {
+            return
+                from product in Products
+                group product by product.CategoryId
+                into prodByCatGroup
+                orderby prodByCatGroup.Key
+                select prodByCatGroup;
+        }
+
+        protected string GetCategoryName(IGrouping<int, ProductDto> groupedProductDtos)
+        {
+            return groupedProductDtos.FirstOrDefault(pg => pg.CategoryId == groupedProductDtos.Key).CategoryName;
+        }
+    }
 }
